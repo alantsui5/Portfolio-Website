c6127ecf7018291895b6e4c8f40574cb5e19be74
Alan Tsui <alantsui2022@outlook.com>
2023-01-05 16:30:58 +0800

    Resolved NextJS 13 Issue

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -3,11 +3,11 @@ import { ThemeProvider } from 'styled-components';
 import theme from "../themes/default";
 import GlobalStyles from './globals';
 
-const Theme = ({ children }) => (
-  <ThemeProvider theme={theme}>
-    <GlobalStyles />
-    {children}
-  </ThemeProvider>
-);
-
-export default Theme;
+export default function Theme({ children }){
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      {children}
+    </ThemeProvider>
+  );
+}
