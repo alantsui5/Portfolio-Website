02d9a5e8e78efe58fc6aff5387b90078234f598d
Alan Tsui <alantsui2022@outlook.com>
2023-01-06 21:39:16 +0800

    Implemented Project Section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,10 +4,39 @@ import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag,
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { projects } from '../../constants/constants';
 
+
+
 export default function Projects(){
   return (
-    <div>
-      Projects
-    </div>
+    <Section nopadding id="projects">
+      <SectionDivider />
+      <SectionTitle main>Projects</SectionTitle>
+      <GridContainer>
+        {projects.map(({id, image, title, description}) =>(
+          <BlogCard key={id}>
+            <Img src={image}/>
+            <TitleContent>
+              <HeaderThree>{title}</HeaderThree>
+              <Hr />
+            </TitleContent>
+            <CardInfo>
+              {description}
+            </CardInfo>
+            <div>
+              <TitleContent>Stack</TitleContent>
+              <tagList>
+                {tags.map((tag, i)=>(
+                  <Tag key={i}> {tag} </Tag>
+                ))}
+              </tagList>
+            </div>
+            <UtilityList>
+              <ExternalLinks href={visit}>Visit</ExternalLinks>
+              <ExternalLinks href={source}>Source</ExternalLinks>
+            </UtilityList>
+          </BlogCard>
+        ))}
+      </GridContainer>
+    </Section>
   );
 }
