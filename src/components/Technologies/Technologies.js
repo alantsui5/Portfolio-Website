25a08db5d8650c5c15b4a3f36f5e9969240c7376
Alan Tsui <alantsui2022@outlook.com>
2023-01-06 23:48:28 +0800

    Implement Technologies Section

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,41 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { DiFirebase, DiPython, DiReact } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 export default function Technologies(){
   return (
-    <div>
-      Technologies
-    </div>
+    <Section id="technologies">
+      <SectionDivider />
+      <br />
+      <SectionTitle>Technologies</SectionTitle>
+      <SectionText>
+        I've worked with a range of web technologies in the web development world.
+        From Backend to Design.
+      </SectionText>
+      <List>
+        <ListItem>
+          <DiReact size="3rem" />
+          <ListContainer>
+            <ListTitle>Front-End</ListTitle>
+            <ListParagraph>Experience with React and Vue</ListParagraph>
+          </ListContainer>
+        </ListItem>
+        <ListItem>
+          <DiFirebase size="3rem" />
+          <ListContainer>
+            <ListTitle>Back-End</ListTitle>
+            <ListParagraph>Experience with SQL and Java Spring, ELK</ListParagraph>
+          </ListContainer>
+        </ListItem>
+        <ListItem>
+          <DiPython size="3rem" />
+          <ListContainer>
+            <ListTitle>Data Analysis</ListTitle>
+            <ListParagraph>Experience with Python, Excel, Clickhouse</ListParagraph>
+          </ListContainer>
+        </ListItem>
+      </List>
+    </Section>
   )
 }
