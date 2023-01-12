c11dd66fd9faa1b853192da9145ad98334a50de3
Alan Tsui <alantsui2022@outlook.com>
2023-01-12 13:41:23 +0000

    Fix CTA Button Styles and Action

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,20 +2,26 @@ import React from 'react';
 
 import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
-import { CallForAction, LeftSection } from './HeroStyles';
+import { LeftSection, ButtonLink } from './HeroStyles';
+import {BiChevronsDown} from 'react-icons/bi';
 
-export default function Hero(props){
+export default function Hero(){
   return (
     <Section row padding>
       <LeftSection>
         <SectionTitle main center>
-          Welcome to  <br />
-          My Personal Portfolio
+          Hi There, <br />
+          I am Alan Tsui
         </SectionTitle>
         <SectionText>
-          Familiar with JavaScript, React, Java, SQL
+         Thrive to work on end-to-end projects which develop sustainable and scalable financial and technical systems to create impact. <br />
+         Have hands-on experience on JavaScript, Testing, Product Design, Data Analysis.
         </SectionText>
-        <CallForAction onClick={() => window.location = "https://alantsui-resume.netlify.app/"}>Click for Resume</CallForAction>
+        <Button>
+            <ButtonLink href="#projects" scroll={false}>
+            Learn More <BiChevronsDown size="3rem"/>
+            </ButtonLink>
+        </Button>
       </LeftSection>
       
     </Section>
--- src/components/Hero/HeroStyles.js
+++ src/components/Hero/HeroStyles.js
@@ -1,24 +1,25 @@
 import styled from 'styled-components';
-import Button from '../../styles/GlobalComponents/Button';
+import Link from 'next/link';
 
 export const LeftSection = styled.div`
   width: 100%;
   @media ${(props) => props.theme.breakpoints.sm} {
-    width: 90%;
+    width: 70%;
     display: flex;
     flex-direction: column;
     margin: 0 auto;
   }
   @media ${(props) => props.theme.breakpoints.md} {
-    width: 90%;
+    width: 70%;
     display: flex;
     flex-direction: column;
     margin: 0 auto;
   }
 `;
 
-export const CallForAction = styled(Button)`
-  @media ${(props) => props.theme.breakpoints.md} {
-    width:90%;
-  }
+
+export const ButtonLink = styled(Link)`
+  color: white;
+  display:flex;
+  alignItems:center;
 `
