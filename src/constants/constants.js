73b82eb616bd1422e6f5948d657d3b5b2c8f27cb
Alan Tsui <33037271+alantsui5@users.noreply.github.com>
2023-01-14 18:42:02 +0800

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -38,9 +38,11 @@ export const projects = [
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: 'Started my journey', },
-  { year: 2018, text: 'Worked as a freelance developer', },
-  { year: 2019, text: 'Founded JavaScript Mastery', },
-  { year: 2020, text: 'Shared my projects with the world', },
-  { year: 2021, text: 'Started my own platform', },
+  { year: 2017, text: 'Study as a computer engineer in Chinese University of Hong Kong', },
+  { year: 2018, text: 'Landed internship in PCCW', },
+  { year: 2019, text: 'Developed 3D space', },
+  { year: 2020, text: 'Developed serverless full-stack application Shoppy. Space Shooter in C++, ASCII Render Engine in C#', },
+  { year: 2020, text: 'Landed internship in Just A Moment', },
+  { year: 2021, text: 'Started my journey as a product manager in Tencent', },
+  { year: 2022, text: 'Participated in internal ERP and App feature design and followed CRP project', },
 ];
