ccab106decb8c1f3f2eb9d32a9c30acb63bf9747
Alan Tsui <33037271+alantsui5@users.noreply.github.com>
2023-01-31 23:14:47 +0800

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -12,7 +12,7 @@ export const projects = [
     title: "Express TypeScript JWT Project",
     description: "Provide a light TypeScript implementation on JWT.",
     image: '/images/expressts-1.png',
-    tags: ["NodeJS 8", "JWT", 'Express', "TypeScript", "TypeORM", "Postgres"],
+    tags: ["NodeJS 8", "TypeScript", "JWT", 'Express',  "TypeORM", "Postgres"],
     source:'https://github.com/alantsui5/express-template',
     visit: 'https://github.com/alantsui5/express-template',
     id: 1,
